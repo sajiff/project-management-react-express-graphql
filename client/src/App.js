0cdd4af72c098f68fc17dc6a12c6a0d6861834ab
sajid <anindasajid@gmail.com>
2022-09-24 01:22:34 +0600

    gh pages

--- client/src/App.js
+++ client/src/App.js
@@ -25,7 +25,7 @@ const cache = new InMemoryCache({
 });
 
 const client = new ApolloClient({
-  uri: "http://localhost:5000/graphql",
+  uri: "https://project-management-react-express-graphql-client-uihz.vercel.app/graphql",
   cache,
 });
 
--- server/index.js
+++ server/index.js
@@ -26,4 +26,10 @@ app.use(
   })
 );
 
+// app.use(express.static("public"));
+
+// app.get("*", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "public", "index.html"));
+// });
+
 app.listen(port, console.log(`server running on ${port}`));
